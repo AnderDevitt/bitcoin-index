1aaa9fe757bc52e2d17cd8162500ae29590b636a
andrew <gcab022116@coderacademy.edu.au>
2022-07-20 17:57:19 +1000

    bitcoinData passed to DisplayData component as state and displays date and value for the object; useEffect hook coded to take currency and fetch data from the API for that currency every time currency is changed

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import CurrencySelector from './CurrencySelector'
 import DisplayData from './DisplayData'
 
@@ -6,7 +6,21 @@ import DisplayData from './DisplayData'
 const App = () => {
   // define state using hooks
   const initialCurrency = "AUD"
+  // const initialData = {"2022-04-18":39036.355,"2022-04-19":40661.67}
   const [currency, setCurrency] = useState(initialCurrency) 
+  const [bitcoinData, setBitcoinData] = useState({})
+  const url = "https://api.coindesk.com/v1/bpi/historical/close.json?currency="
+  
+  useEffect( () => {
+    console.log("Hi, I'm using useEfect")
+    fetch(`${url}${currency}`)
+    .then(response => response.json())
+    .then(data => setBitcoinData(data.bpi))
+    .catch(e => console.log(e))
+  }
+  , 
+  [currency]
+  )
 
   const handleCurrency = (value) => {
       console.log(value)
@@ -17,7 +31,7 @@ const App = () => {
     <div >
           <h1>Bitcoin Index</h1>
           <CurrencySelector currency={currency} handleCurrency={handleCurrency}/>
-          <DisplayData />
+          <DisplayData bitcoinData={bitcoinData}/>
     </div>
   )
 }
--- src/components/DisplayData.js
+++ src/components/DisplayData.js
@@ -1,7 +1,12 @@
-const DisplayData = () => {
+const DisplayData = ({bitcoinData}) => {
     return (
         <div>
-
+            {/* use object dot keys to iterate through the object with the map function */}
+            {Object.keys(bitcoinData).map(date =>
+                <div key={date}>
+                    Date: {date} Value: {bitcoinData[date]}
+                </div>
+            )}
         </div>
     )
 }
