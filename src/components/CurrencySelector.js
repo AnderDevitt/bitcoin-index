db883314b7572d5987e8dd84533f3a8fcde02cc0
andrew <gcab022116@coderacademy.edu.au>
2022-07-20 15:51:06 +1000

    CurrencySelector component renders list of currencies

--- src/components/CurrencySelector.js
+++ src/components/CurrencySelector.js
@@ -1,7 +1,15 @@
+import currencies from '../data/supported-currencies.json'
+
+
 const CurrencySelector = () => {
     return (
         <div>
-
+            <span>Select your currency: </span>
+            <select>
+                {currencies.map(obj =>
+                    <option>{obj.country}</option>    
+                )}
+            </select>
         </div>
     )
 }
